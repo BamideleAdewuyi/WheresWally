8b5b1b17591a0b8dae46e411bb825c54e6d0febc
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-08-31 17:45:15 +0100

    Client/Frontend/src/components/Homepage/Homepage.jsx: Added return

---
+++ Client/Frontend/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+function Homepage() {
+    return(
+        <>
+            <h1>Where's Wally?</h1>
+            <h2>Find all these characters, as fast as you can</h2>
+            <div>
+
+            </div>
+            <Link>Start Game</Link>
+        </>
+    )
+}
+
+export default Homepage;

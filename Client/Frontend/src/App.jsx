fac960b59d046d028a10743c96ba33b9bc254497
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-08-31 15:18:35 +0100

    Client/Frontend/src/App.jsx: Imported Link and Outlet. Wrote basic return elements

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Link, Outlet } from "react-router";
 import './App.css'
 
 function App() {
   return (
     <>
-      
+      <h1>Where's Wally?</h1>
+      <h2>Find all these characters, as fast as you can</h2>
+      <div>
+
+      </div>
+      <Link>Start Game</Link>
+      <Outlet/>
     </>
   )
 }

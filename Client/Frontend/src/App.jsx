c72fc69cb2793765e78fd6b94439301f78fbb398
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-08-31 17:31:17 +0100

    Client/Frontend/src/App.jsx: Removed everything except Outlet element

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -4,12 +4,6 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Where's Wally?</h1>
-      <h2>Find all these characters, as fast as you can</h2>
-      <div>
-
-      </div>
-      <Link>Start Game</Link>
       <Outlet/>
     </>
   )

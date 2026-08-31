968468d7994dc55ce0eb1ce987eec0f9f57bca60
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-01 00:43:19 +0100

    Client/Frontend/src/components/Homepage/Homepage.jsx: Imported images of characters. Imported LargeCharacter component. Rendered images in return

--- Client/Frontend/src/components/Homepage/Homepage.jsx
+++ Client/Frontend/src/components/Homepage/Homepage.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router";
+import LargeCharacter from "../LargeCharacter/LargeCharacter";
+import Wally from "../../assets/Wally.png";
+import Odlaw from "../../assets/Odlaw.webp";
+import Wanda from "../../assets/Wanda.webp";
+import Whitebeard from "../../assets/Whitebeard.webp"
 
 function Homepage() {
+    const characters = [Wally, Odlaw, Wanda, Whitebeard];
     return(
         <>
             <h1>Where's Wally?</h1>
             <h2>Find all these characters, as fast as you can</h2>
             <div>
-
+                {characters.map((character, index) => (
+                    <LargeCharacter key={index} src={character} name={character} />
+                ))}
             </div>
             <Link>Start Game</Link>
         </>

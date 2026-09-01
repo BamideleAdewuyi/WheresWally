b239e38b6e179696d36d400526af262ae8304087
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-01 15:05:38 +0100

    Client/Frontend/src/components/LargeCharacter/LargeCharacter.jsx: Added h3 element that displays name of character

--- Client/Frontend/src/components/LargeCharacter/LargeCharacter.jsx
+++ Client/Frontend/src/components/LargeCharacter/LargeCharacter.jsx
@@ -2,7 +2,10 @@ import styles from "./LargeCharacter.module.css";
 
 function LargeCharacter({ src, name }) {
     return(
-        <img className={styles.largeCharacter} src={src} alt={`image of ${name}`} />
+        <>
+            <img className={styles.largeCharacter} src={src} alt={`image of ${name}`} />
+            <h3 className={styles.largeCharacterName}>{name}</h3>
+        </>
     )
 }
 

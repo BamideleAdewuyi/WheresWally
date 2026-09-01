ddc47ed30431d5fa6760ad2b08bb5701575c933b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-01 20:00:54 +0100

    Client/Frontend/src/components/Router/Router.jsx: Imported Game and Homepage components. Added paths

--- Client/Frontend/src/components/Router/Router.jsx
+++ Client/Frontend/src/components/Router/Router.jsx
@@ -1,4 +1,6 @@
 import App from "../../App";
+import Game from "../Game/Game";
+import Homepage from "../Homepage/Homepage";
 import { createBrowserRouter } from 'react-router';
 
 const router = createBrowserRouter([
@@ -6,7 +8,8 @@ const router = createBrowserRouter([
         path: "/",
         element: <App/>,
         children: [
-
+            {path: "/", element: <Homepage />},
+            {path: "/play", element: <Game />},
         ]
     }
 ]);

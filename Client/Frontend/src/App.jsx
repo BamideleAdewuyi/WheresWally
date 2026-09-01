037b7f27cec98069776d5b2fb94b3ec4e812e39b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-01 20:00:15 +0100

    Client/Frontend/src/App.jsx: Removed Homepage component

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router";
-import Homepage from "./components/Homepage/Homepage";
 import './App.css'
 
 function App() {
   return (
     <>
-      <Homepage/>
       <Outlet/>
     </>
   )

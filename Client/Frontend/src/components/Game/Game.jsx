c34d1ae0a95a3a5bdadf5f3a7dff2fc182fb4b99
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-03 19:04:53 +0100

    Client/Frontend/src/components/Game/Game.jsx: Imported useRef, useEffect and useState. Added state variable for dimensions. Set up useEffect to observe resize of gameContainer. Handle click function calculates location of click relative to gameContainer

--- Client/Frontend/src/components/Game/Game.jsx
+++ Client/Frontend/src/components/Game/Game.jsx
@@ -1,9 +1,38 @@
 import styles from "./Game.module.css";
+import { useRef, useEffect, useState } from "react";
 
 function Game() {
+    const gameContainerRef = useRef(null);
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        if (gameContainerRef.current) {
+            const resizeObserver = new ResizeObserver((entries) => {
+                for (let entry of entries) {
+                    setDimensions({
+                        width: entry.contentRect.width,
+                        height: entry.contentRect.height,
+                    });
+                }
+            });
+            resizeObserver.observe(gameContainerRef.current);
+            return () => {
+                resizeObserver.disconnect();
+            }
+        }
+    }, []);
+
+    function handleClick(e) {
+        const x = e.clientX - e.currentTarget.getBoundingClientRect().left;
+        const y = e.clientY - e.currentTarget.getBoundingClientRect().top;
+
+        const xCoord = Math.floor(x / dimensions.width * 10000)/100;
+        const yCoord = Math.floor(y / dimensions.height * 10000)/100;
+        console.log(xCoord, yCoord)
+    }
     return(
         <div className={styles.screenWrapper}>
-            <div className={styles.gameContainer}>
+            <div onClick={(e) => handleClick(e)} ref={gameContainerRef} className={styles.gameContainer}>
             </div>
         </div>
     )

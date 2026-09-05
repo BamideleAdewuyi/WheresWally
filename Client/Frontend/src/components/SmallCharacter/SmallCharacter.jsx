89aff1cce2568d67887a057505be8a0b33a81627
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-05 03:07:12 +0100

    Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx: Added container around images and text

--- Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
+++ Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
@@ -2,10 +2,10 @@ import styles from "./SmallCharacter.module.css";
 
 function SmallCharacter({ src, name }) {
     return(
-        <>
+        <div className={styles.smallCharacterContainer}>
             <img className={styles.smallCharacter} src={src} alt={`image of ${name}`} />
             <h3 className={styles.smallCharacterName}>{name}</h3>
-        </>
+        </div>
     )
 }
 

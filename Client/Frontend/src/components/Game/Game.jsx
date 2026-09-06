6f7a4b9c3cea0520af29f719ea0e683daae0fb8d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-06 03:26:33 +0100

    Client/Frontend/src/components/Game/Game.jsx: handleClick now only runs if box is not open. Wrote closeBox function and passed TargetBox component

--- Client/Frontend/src/components/Game/Game.jsx
+++ Client/Frontend/src/components/Game/Game.jsx
@@ -26,29 +26,31 @@ function Game() {
     }, []);
 
     function handleClick(e) {
-        if (open) {
-            setOpen(false);
-        } else {
+        if (!open) {
             setOpen(true);
+    
+            const x = e.clientX - e.currentTarget.getBoundingClientRect().left;
+            const y = e.clientY - e.currentTarget.getBoundingClientRect().top;
+    
+            const safeWidth = window.innerWidth - 168;
+            const safeHeight = window.innerHeight - 168;
+            const realBoxX = Math.min(safeWidth, e.clientX);
+            const realBoxY = Math.min(safeHeight, e.clientY);
+    
+            const xCoord = Math.floor(x / dimensions.width * 10000)/100;
+            const yCoord = Math.floor(y / dimensions.height * 10000)/100;
+            console.log(xCoord, yCoord)
+            setBoxPosition({ x: realBoxX, y: realBoxY });
         }
+    }
 
-        const x = e.clientX - e.currentTarget.getBoundingClientRect().left;
-        const y = e.clientY - e.currentTarget.getBoundingClientRect().top;
-
-        const safeWidth = window.innerWidth - 168;
-        const safeHeight = window.innerHeight - 168;
-        const realBoxX = Math.min(safeWidth, e.clientX);
-        const realBoxY = Math.min(safeHeight, e.clientY);
-
-        const xCoord = Math.floor(x / dimensions.width * 10000)/100;
-        const yCoord = Math.floor(y / dimensions.height * 10000)/100;
-        console.log(xCoord, yCoord)
-        setBoxPosition({ x: realBoxX, y: realBoxY });
+    function closeBox() {
+        setOpen(false);
     }
     return(
         <div className={styles.screenWrapper}>
             <div onClick={(e) => handleClick(e)} ref={gameContainerRef} className={styles.gameContainer}>
-                <TargetBox open={open} x={boxPosition.x} y={boxPosition.y}/>
+                <TargetBox open={open} closeBox={closeBox} x={boxPosition.x} y={boxPosition.y}/>
             </div>
         </div>
     )

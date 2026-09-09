1a319c6de690480e44eb10b84234414208331d3a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-10 00:03:38 +0100

    db/queries.js: Wrote and exported findCharacterByCoordinates function

--- db/queries.js
+++ db/queries.js
@@ -3,8 +3,23 @@ const prisma = require("../lib/prisma.js");
 async function findAllUsers() {
     const users = await prisma.user.findMany();
     return users;
-}
+};
+
+async function findCharacterByCoordinates({ x, y, name }) {
+    const character = await prisma.character.findFirst({
+        where: { 
+            name: name,
+            minX: { lte: x },
+            maxX: { gte: x },
+            minY: { lte: y },
+            maxY: { gte: y },
+         },
+    });
+
+    return character;
+};
 
 module.exports = {
     findAllUsers,
+    findCharacterByCoordinates,
 }

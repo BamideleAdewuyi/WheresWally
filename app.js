fbf54a11202fb305850296719fcb305ae0b6ea04
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-10 16:36:00 +0100

    app.js: Imported jsonwebtoken

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("node:path");
 const router = require("./routes/index.js");
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
 

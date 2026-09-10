7c3e3cb11bb1dd35728712c6df62e741327fd3c4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-10 17:39:13 +0100

    routes/game.js: Imported jsonwebtoken. Wrote /start route that sets cookie using jwt

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,24 @@
 const { Router } = require("express");
 const gameController = require("../controllers/gameController");
 const gameRouter = Router();
+const jwt = require('jsonwebtoken');
+
+gameRouter.get("/start", function (req, res, next) {
+    const gameObj = {
+        characters: {
+            Wally: false,
+            Wanda: false,
+            Odlaw: false,
+            Whitebeard: false,
+        },
+        startTime: Date.now(),
+    };
+
+    const token = jwt.sign(gameObj, process.env.JWT_SECRET);
+
+    res.cookie("gameCookie", token, { httpOnly: true, secure: true, sameSite: 'lax' });
+    return res.send("Cookie set");
+});
 
 gameRouter.get("/allUsers", gameController.allUsersGet);
 

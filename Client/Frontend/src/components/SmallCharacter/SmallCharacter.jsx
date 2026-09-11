220f5aa21d2f89f68cad24c45d1771cf2749be5b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-11 01:11:35 +0100

    Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx: Corrected handleClick

--- Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
+++ Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
@@ -2,7 +2,7 @@ import styles from "./SmallCharacter.module.css";
 
 function SmallCharacter({ src, name, handleClick }) {
     return(
-        <div onClick={(name) = handleClick(name)} className={styles.smallCharacterContainer}>
+        <div onClick={() => handleClick(name)} className={styles.smallCharacterContainer}>
             <img className={styles.smallCharacter} src={src} alt={`image of ${name}`} />
             <h3 className={styles.smallCharacterName}>{name}</h3>
         </div>

1cfc269b9e2aeb57b93f65f1efab51ffe6d688d1
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-12 15:18:21 +0100

    app.js: Added object to app.use(cors)

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3002",
+    credentials: true,
+}));
 app.use(cookieParser());
 app.use("/", router);
 

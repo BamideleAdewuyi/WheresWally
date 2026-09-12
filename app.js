3fc62820bf79c3531ba1578160bb50fcde97c808
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-12 02:40:36 +0100

    app.js: Set app to use cookie-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("node:path");
 const router = require("./routes/index.js");
 const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use("/", router);
 
 

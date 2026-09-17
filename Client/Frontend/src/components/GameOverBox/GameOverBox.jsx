4aa8f99498f2d5bf2bcbd959333adc239229610a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-17 21:16:07 +0100

    Client/Frontend/src/components/GameOverBox/GameOverBox.jsx: Wrote and exported GameOverBox component. Has dialog box containing form for adding usernames to leaderboard

---
+++ Client/Frontend/src/components/GameOverBox/GameOverBox.jsx
@@ -0,0 +1,33 @@
+import { useRef, useEffect } from "react";
+import styles from "./GameOverBox.module.css"
+
+function GameOverBox({ open, x, y, time }) {
+    const dialogRef = useRef();
+
+    useEffect(() => {
+        if (dialogRef.current) {
+            if (open) {
+                dialogRef.current.showModal();
+            } else {
+                dialogRef.current.close();
+            }
+        }
+    }, [open]);
+
+    return(
+        <div className={styles.GameOverBoxWrapper}>
+            <dialog style={{left: x, top: y}} ref={dialogRef} className={styles.GameOverBoxDialog}>
+                <form onSubmit>
+                    <h1>Well done! You found everyone in {time}</h1>
+                    <h2>Add your name to the leaderboard:</h2>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name"/>
+                    <button type="submit"></button>
+                    <button type="button">Go home without saving</button>
+                </form>
+            </dialog>
+        </div>
+    )
+}
+
+export default GameOverBox

fea085a88b8a8b94db299917e7e09521101e31a3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-18 00:57:57 +0100

    Client/Frontend/src/components/GameOverBox/GameOverBox.jsx: Imported useState. Set up useEffect for getting the user time. Set up state variable for time. Rendered time in form

--- Client/Frontend/src/components/GameOverBox/GameOverBox.jsx
+++ Client/Frontend/src/components/GameOverBox/GameOverBox.jsx
@@ -1,8 +1,9 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import styles from "./GameOverBox.module.css"
 
 function GameOverBox({ x, y }) {
     const dialogRef = useRef();
+    const [time, setTime] = useState(null);
 
     useEffect(() => {
         async function getCookie() {
@@ -20,6 +21,27 @@ function GameOverBox({ x, y }) {
     }, []);
 
     useEffect(() => {
+        async function getTime() {
+            try {
+                const res = await fetch(`${import.meta.env.VITE_API_PORT}/time`, {
+                method: "GET",
+                headers: { "content-type": "application/json", },
+                credentials: "include",
+            });
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+
+            const data = await res.json();
+            setTime(data.time);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        getTime();
+    }, []);
+
+    useEffect(() => {
         dialogRef.current.showModal();
     }, []);
 
@@ -27,7 +49,7 @@ function GameOverBox({ x, y }) {
         <div className={styles.GameOverBoxWrapper}>
             <dialog style={{left: x, top: y}} ref={dialogRef} className={styles.GameOverBoxDialog}>
                 <form onSubmit>
-                    <h1>Well done! You found everyone in </h1>
+                    <h1>Well done! You found everyone in {time}</h1>
                     <h2>Add your name to the leaderboard:</h2>
                     <label htmlFor="name">Name</label>
                     <input type="text" id="name" name="name"/>

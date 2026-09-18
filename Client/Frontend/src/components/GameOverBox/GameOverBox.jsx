ae4b1fc35132ee17ede0ab69fad07b014cf8b877
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-18 23:15:09 +0100

    Client/Frontend/src/components/GameOverBox/GameOverBox.jsx: Imported useNavigate. Declared state variables name and erros. Set up useNavigate variable. Wrote addUser function. Called on form submit

--- Client/Frontend/src/components/GameOverBox/GameOverBox.jsx
+++ Client/Frontend/src/components/GameOverBox/GameOverBox.jsx
@@ -1,8 +1,12 @@
 import { useRef, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import styles from "./GameOverBox.module.css"
 
 function GameOverBox({ x, y, time }) {
     const dialogRef = useRef();
+    const [name, setName] = useState('');
+    const [errors, setErrors] = ([]);
+    const navigate = useNavigate();
 
     function msToTime(duration) {
         let milliseconds = Math.floor((duration % 1000) / 100),
@@ -27,14 +31,39 @@ function GameOverBox({ x, y, time }) {
         dialogRef.current.showModal();
     }, []);
 
+    async function addUser(e) {
+        e.preventDefault();
+        const userObj = { name };
+        try {
+            const res = await fetch(`${import.meta.env.VITE_API_PORT}/newUser`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify(userObj)
+        });
+        if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json();
+        if (data.msg) {
+            console.log(data.msg);
+            navigate('/');
+        } else {
+            setErrors(data.errors.map((error) => error.msg));
+        }    
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
         <div className={styles.GameOverBoxWrapper}>
             <dialog style={{left: x, top: y}} ref={dialogRef} className={styles.GameOverBoxDialog}>
-                <form onSubmit>
+                <form onSubmit={addUser}>
                     <h1>Well done! You found everyone in {formattedTime}</h1>
                     <h2>Add your name to the leaderboard:</h2>
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name"/>
+                    <input value={name} type="text" id="name" name="name" onChange={e => setName(e.target.value)}/>
                     <button type="submit">Save score</button>
                     <button type="button">Go home without saving</button>
                 </form>

81a38f5164660d317c485a5a09acabc160270b47
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-19 03:45:32 +0100

    Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx: Added ticked prop that determines the tick display styling

--- Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
+++ Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
@@ -1,11 +1,11 @@
 import styles from "./SmallCharacter.module.css";
 import Tick from "../../assets/Tick.png";
 
-function SmallCharacter({ src, name, handleClick }) {
+function SmallCharacter({ src, name, handleClick, ticked = "none" }) {
     return(
         <div onClick={() => handleClick(name)} className={styles.smallCharacterContainer}>
             <img className={styles.smallCharacter} src={src} alt={`image of ${name}`} />
-            <img src={Tick} alt="tick" className={styles.tick}/>
+            <img src={Tick} alt="tick" className={styles.tick} style={{display: ticked}}/>
             <h3 className={styles.smallCharacterName}>{name}</h3>
         </div>
     )

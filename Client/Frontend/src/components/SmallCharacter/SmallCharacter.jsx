038fa520c6f7a1123373befdd188cab62a85c0c4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-19 16:13:03 +0100

    Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx: If ticked prop is true, tick image is displayed. Otherwise it is invisible

--- Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
+++ Client/Frontend/src/components/SmallCharacter/SmallCharacter.jsx
@@ -5,7 +5,7 @@ function SmallCharacter({ src, name, handleClick, ticked = "none" }) {
     return(
         <div onClick={() => handleClick(name)} className={styles.smallCharacterContainer}>
             <img className={styles.smallCharacter} src={src} alt={`image of ${name}`} />
-            <img src={Tick} alt="tick" className={styles.tick} style={{display: ticked}}/>
+            <img src={Tick} alt="tick" className={styles.tick} style={{display: ticked == true ? "block" : "none"}}/>
             <h3 className={styles.smallCharacterName}>{name}</h3>
         </div>
     )

db340db0d5a6be14c600c0bd2da5e8e0a8d03953
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-19 23:19:11 +0100

    controllers/gameController.js: Rewrote takeTurnPost so it uses try/catch and has a way out if character does not return

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -25,34 +25,37 @@ async function takeTurnPost(req, res) {
     const name = req.body.name;
 
     const character =  await db.findCharacterByCoordinates({ x, y, name });
-    const token = req.cookies.gameCookie;
-    const sessionData = jwt.verify(token, process.env.JWT_SECRET);
-    if (character) {
-        const startTime = sessionData.startTime;
-        sessionData.characters[character.name] = true;
-        const characters = sessionData.characters;
-        const gameOver = Object.values(characters).every(character => character === true);
-        const time = gameOver ? Date.now() - startTime : null;
-        const gameObj = {
-            characters: characters,
-            startTime: startTime,
-            gameOver: gameOver,
-            time: time,
-        };
-        const newToken = jwt.sign(gameObj, process.env.JWT_SECRET);
-        
-        res.cookie("gameCookie", newToken, { httpOnly: true, secure: true, sameSite: 'lax' });
-        res.json({
-            character: character,
-            sessionData: gameObj,
-        });
-        return;
+    if (!character) {
+      return res.json({ 
+          character: null, 
+          sessionData: null
+      });
     }
+    try {
+      const token = req.cookies.gameCookie;
+      const sessionData = jwt.verify(token, process.env.JWT_SECRET);
+      const startTime = sessionData.startTime;
+      sessionData.characters[character.name] = true;
+      const characters = sessionData.characters;
+      const gameOver = Object.values(characters).every(character => character === true);
+      const time = gameOver ? Date.now() - startTime : null;
+      const gameObj = {
+          characters: characters,
+          startTime: startTime,
+          gameOver: gameOver,
+          time: time,
+      };
+      const newToken = jwt.sign(gameObj, process.env.JWT_SECRET);
 
-    res.json({ 
-        character: character,
-        sessionData: sessionData,
-    });
+      res.cookie("gameCookie", newToken, { httpOnly: true, secure: true, sameSite: 'none' });
+      res.json({
+          character: character,
+          sessionData: gameObj,
+      });
+
+    } catch(err) {
+      return res.status(401).json({ error: "Invalid or expired session token"})
+    }
 };
 
 async function timeGet(req, res) {

c17a48fd17a851027b0c9211c028799e805ebbf3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-09-19 22:40:08 +0100

    app.js: app uses environment variable in cors, if available

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('assets'));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3002",
+    origin: process.env.FRONTEND_URL || "http://localhost:3002",
     credentials: true,
 }));
 app.use(cookieParser());
